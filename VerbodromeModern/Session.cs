90c9c6224ab57feb2336623182aa67c8ab5e53a4
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-06-06 21:51:28 +0200

    Add probability-based question selection

--- VerbodromeModern/Session.cs
+++ VerbodromeModern/Session.cs
@@ -33,9 +33,31 @@ namespace VerbodromeModern
         public Question GenerateRandomQuestion(string[] verbs, string[] tenses, string[] persons)
         {
             var verb = verbs[_random.Next(verbs.Length)];
-            var tense = tenses[_random.Next(tenses.Length)];
+            var tense = ChooseTense(tenses);
             var person = persons[_random.Next(persons.Length)];
             return new Question(verb, tense, person);
+        }
+
+        private string ChooseTense(string[] tenses)
+        {
+            var stats = new (string Tense, double Accuracy)[tenses.Length];
+            for (int i = 0; i < tenses.Length; i++)
+            {
+                var (c, t) = Score.StatsForTense(tenses[i]);
+                double acc = t == 0 ? 0 : (double)c / t;
+                stats[i] = (tenses[i], acc);
+            }
+            Array.Sort(stats, (a, b) => b.Accuracy.CompareTo(a.Accuracy));
+            int sum = tenses.Length * (tenses.Length + 1) / 2;
+            int pick = _random.Next(sum) + 1; // 1..sum
+            int cumulative = 0;
+            for (int i = 0; i < stats.Length; i++)
+            {
+                cumulative += i + 1;
+                if (pick <= cumulative)
+                    return stats[i].Tense;
+            }
+            return stats[^1].Tense;
         }
 
         public void RecordAnswer(Question question, bool correct)
